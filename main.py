51ed3a7e0ffc63a7a816c45a5c00a6cdf9ce87eb
antipups <nikitakurkurin@protonmail.ch>
2019-12-01 17:24:17 +0300

    Сделано: 1) Нормальная работа с спец. символами html, теперь для них работают рег. выражения и работает всё нормально 2) Добавлен парсинг выпусков, полностью, кроме категорий.

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+import re
+
 import requests
 
 
 def encode_from_html(string):   # перекодировка из html символов в обычные
-    return ''.join(chr(int(x[-4:])) for x in string.split(';') if len(x) > 1)
+    while re.search(r'&#\d{1,4};', string) is not None:
+        swap_word = re.search(r'&#\d{1,4};', string)[0]    # копируем изменяемое слово
+        if len(swap_word) != 7 or not 1040 <= int(swap_word[-5:-1]) <= 1103:  # все слова которые не буквы, меняем на пробел
+            new_word = ' '
+        else:
+            new_word = chr(int(re.search(swap_word, string)[0][-5:-1]))
+        string = re.sub(swap_word, new_word, string)
+    return string
 
 
 def parse(url):
@@ -31,24 +40,49 @@ def parse(url):
 
     # находим ключевые слова если они есть
     keyword_of_podcasts = str()
-    if html.find('keywords>') > -1:
-        temp_html = html[html.find('keywords>') + 9:]  # временная срезка, для нахождения ключ. слов
+    if html.find('keywords>') > -1:     # если есть ключевые слова
+        temp_html = html[html.find('keywords>') + 9:]   # временная срезка, для нахождения ключ. слов
         keyword_of_podcasts += temp_html[: temp_html.find('</')]
-        new_keywords = str()
-        for word in keyword_of_podcasts.split(','):
-            if word.find('#') > -1:
-                new_keywords += encode_from_html(word) + ', '
-            else:
-                new_keywords += word + ', '
-        keyword_of_podcasts = new_keywords
+        if keyword_of_podcasts.find('#') > -1:
+            keyword_of_podcasts = encode_from_html(keyword_of_podcasts)
 
-    print(title_of_podcast, description_of_podcast, image_of_podcasts, keyword_of_podcasts)
+    # print(title_of_podcast, description_of_podcast, image_of_podcasts, keyword_of_podcasts)
 
+    html = html[html.find('<item>'):]   # так как информация о подкасте уже собрана, обрезаем её и начинаем собирать инфу о выпусках
+
+    ls_of_items = []    # впредь выпуски будут items, это список со всеми выпусками
+    while html.find('<item>') > -1:    # до тех пор пока находим новый выпуск
+
+        # получаем блок с этим itemом, чтоб работать не по всей странице
+        item_code = html[html.find('<item>') + 7: html.find('</item>')]
+
+        # получаем название выпуска
+        title_of_item = item_code[item_code.find('<title>') + 7: item_code.find('</title>')]
+
+        # получаем описание выпуска
+        description_of_item = item_code[item_code.find('<description>') + 13: item_code.find('</description>')]
+        if description_of_item.find('<![CDATA[') > -1:
+            description_of_item = description_of_item[
+                                     description_of_item.find('<![CDATA[') + 9: description_of_item.find(']]>')]
+
+        # переходим в тег с ссылкой на аудио
+        enclosure = item_code[item_code.find('<enclosure'):]
+        mp3 = enclosure[enclosure.find('url="') + 5: enclosure.find('"/>')]    # получаем аудио
+
+        # получаем дату публикации выпуска
+        pubdata_of_items = item_code[item_code.find('<pubDate>') + 9: item_code.find('</pubDate>')]
+
+        # получаем область с длительностью аудио
+        duration_code = item_code[item_code.find('duration>') + 9: item_code.find('duration>') + 20]
+        duration_of_items = duration_code[:duration_code.find('</')]    # получаем длительность аудио
+
+        html = html[html.find('</item>') + 7:]
+        # print(encode_from_html(title_of_item), '\n', description_of_item, '\n', mp3, '\n', pubdata_of_items, '\n', duration_of_items)
 
 
 if __name__ == '__main__':
-    parse('https://feeds.simplecast.com/CPNlXNwD')
-    parse('https://podster.fm/rss.xml?pid=20066')
-    parse('https://podster.fm/rss.xml?pid=29605')
+    # parse('https://feeds.simplecast.com/CPNlXNwD')
+    # parse('https://podster.fm/rss.xml?pid=20066')
+    # parse('https://podster.fm/rss.xml?pid=29605')
     parse('https://meduza.io/rss/podcasts/peremotka')
-    parse('https://mojomedia.ru/feed-podcasts/rebyata-my-potrahalis')
+    # parse('https://mojomedia.ru/feed-podcasts/rebyata-my-potrahalis')
