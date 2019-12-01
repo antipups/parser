ea3834314478746ec371b0f1b17ad364b5ce647a
antipups <nikitakurkurin@protonmail.ch>
2019-12-01 15:14:48 +0300

    Сделано: 1) Добавлен файл конфига, в нем пока ссылки на бд 2) Добавлена библиотека для работы с БД прописанная нами 3) Сделан парсинг информации о подкасте, картинка, название, описание, ключевые слова

--- main.py
+++ main.py
@@ -1,35 +1,46 @@
-import re
 import requests
-from bs4 import BeautifulSoup
 
 
-def parse_from_mojomedia(url):
+def encode_from_html(string):   # перекодировка из html символов в обычные
+    return ''.join(chr(int(x[-4:])) for x in string.split(';') if len(x) > 1)
+
+
+def parse(url):
     html = requests.get(url).text
-    print(html)
+    # находим название подкаста
+    title_of_podcast = html[html.find('<title>') + 7: html.find('</title>')]
+    if title_of_podcast.find('#') > -1:  # если встретили спецсимволы html
+        title_of_podcast = encode_from_html(title_of_podcast)
 
+    # находим описание подкаста
+    description_of_podcast = html[html.find('<description>') + 13: html.find('</description>')]
+    if description_of_podcast.find('<![CDATA[') > -1:  # если есть cdata (не читаемая интерпр. хуйня)
+        description_of_podcast = description_of_podcast[
+                                 description_of_podcast.find('<![CDATA[') + 9: description_of_podcast.find(']]>')]
 
-def parse_from_simplecast(url):
-    pass
+    # находим картинку подкаста
+    image_of_podcasts = html[html.find('<image>') + 7: html.find('</image>')]
+    image_of_podcasts = image_of_podcasts[image_of_podcasts.find('<url>') + 5: image_of_podcasts.find('</url>')]
 
-
-def parse_from_meduza(url):
-    pass
-
-
-def parse_from_podster(url):
-    pass
-
-
-def change(url):
-    if url.find('mojomedia') > -1:
-        parse_from_mojomedia(url)
-    elif url.find('feeds') > -1:
-        parse_from_simplecast(url)
-    elif url.find('meduza') > -1:
-        parse_from_meduza(url)
-    elif url.find('podster') > -1:
-        parse_from_podster(url)
+    # находим ключевые слова если они есть
+    keyword_of_podcasts = str()
+    if html.find('<itunes:keywords>') > -1:
+        temp_html = html[html.find('keywords>') + 9:]  # временная срезка, для нахождения ключ. слов
+        keyword_of_podcasts += temp_html[: temp_html.find('</')]
+        new_keywords = str()
+        for word in keyword_of_podcasts.split(','):
+            print(word)
+            if word.find('#') > -1:
+                new_keywords += encode_from_html(word) + ', '
+            else:
+                new_keywords += word + ', '
+        keyword_of_podcasts = new_keywords
+    print(title_of_podcast, description_of_podcast, image_of_podcasts, keyword_of_podcasts)
 
 
 if __name__ == '__main__':
-    change('https://mojomedia.ru/feed-podcasts/rebyata-my-potrahalis')
+    # parse('https://feeds.simplecast.com/CPNlXNwD')
+    # parse('https://podster.fm/rss.xml?pid=20066')
+    # parse('https://podster.fm/rss.xml?pid=29605')
+    parse('https://meduza.io/rss/podcasts/peremotka')
+    # parse('https://mojomedia.ru/feed-podcasts/rebyata-my-potrahalis')
---
+++ util.py
@@ -0,0 +1,59 @@
+import pymysql.cursors
+import config
+
+paramstyle = "%s"
+
+
+def deploy_database():
+    """
+     Создать нужные таблицы в базе данных
+    """
+    pass
+
+
+def connect():
+    """
+     Подключение к базе данных
+    """
+    return pymysql.connect(
+        config.db_host,
+        config.db_user,
+        config.db_password,
+        config.db_database,
+        use_unicode=True,
+        charset=config.db_charset,
+        cursorclass=pymysql.cursors.DictCursor)
+
+
+def execute(sql, *args, commit=False):
+    """
+     Формат запроса:
+     execute('<Запрос>', <передаваемые параметры>, <commit=True>)
+    """
+    db = connect()
+    cur = db.cursor()
+    try:
+        cur.execute(sql % {"p": paramstyle}, args)
+    except pymysql.err.InternalError as e:
+        if sql.find('texts') == -1:
+            print('Cannot execute mysql request: ' + str(e))
+        return
+    if commit:
+        db.commit()
+        db.close()
+    else:
+        ans = cur.fetchall()
+        db.close()
+        return ans
+
+
+def set_new_podcast(title_of_podcast, description_of_podcasts, url_of_image_of_podcast):
+    execute('INSERT INTO podcasts (title_of_podcast, description_of_podcast, url_of_image_of_podcast) '
+            'VALUES (%(p)s, %(p)s, %(p)s)', title_of_podcast, description_of_podcasts, url_of_image_of_podcast, commit=True)
+
+
+def set_new_audio(title_of_podcast, title_of_audio, description_of_audio, audio, image_of_audio, pubdata_of_audio, duration_of_audio):
+    id_of_podcast = execute('SELECT id_of_podcast FROM podcasts WHERE title_of_podcast = %(p)s', title_of_podcast)[0].get('id_of_podcast')
+    execute('INSERT INTO audio (id_of_podcast, titile_of_audio, description_of_audio, audio, image_of_audio, pubdata_of_audio, duration_of_audio)'
+            ' VALUES (%(p)s, %(p)s, %(p)s, %(p)s, %(p)s, %(p)s, %(p)s)', id_of_podcast, title_of_audio, description_of_audio, audio, image_of_audio,
+            pubdata_of_audio, duration_of_audio, commit=True)
