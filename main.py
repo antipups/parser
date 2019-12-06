84c3de4dcbe46ed4e771ed463c956c4571e1584c
antipups <nikitakurkurin@protonmail.ch>
2019-12-06 23:08:05 +0300

    Сделано: 1) Сделана подкачка ссылок на url из бдшки; 2) Сделана скачка парсер, его тегов и категорий.

--- main.py
+++ main.py
@@ -91,116 +91,121 @@ def clear_pubdata(string):
     return string[4:8] + string[2:4] + string[:2] + string[-6:]   # подводим под шаблон бд
 
 
-def parse(url):
+def parse():
     """
-
-    """
-    html = requests.get(url).content.decode('utf-8')
-    pre_item_html = html[:html.find('<item>')]
-    # находим название подкаста
-    title_of_podcast = pre_item_html[pre_item_html.find('<title>') + 7: pre_item_html.find('</title>')]
-    title_of_podcast = check_on_shit(title_of_podcast)
-
-    # находим описание подкаста
-    description_of_podcast = parse_description(pre_item_html)
-
-    # находим картинку подкаста
-    if pre_item_html.find('<image>') > -1:
-        image_of_podcasts = pre_item_html[pre_item_html.find('<image>') + 7: pre_item_html.find('</image>')]
-        image_of_podcasts = image_of_podcasts[image_of_podcasts.find('<url>') + 5: image_of_podcasts.find('</url>')]
-    else:
-        image_of_podcasts = pre_item_html[pre_item_html.find('image') + 5:]
-        image_of_podcasts = image_of_podcasts[image_of_podcasts.find('href="') + 6:]
-        image_of_podcasts = image_of_podcasts[: image_of_podcasts.find('"')]
-
-    # находим ключевые слова если они есть
-    keyword_of_podcasts = str()
-    if pre_item_html.find('keywords>') > -1:     # если есть ключевые слова
-        keyword_of_podcasts = parse_keywords(pre_item_html)
-
-    # находим автора, если он есть
-    author_of_podcast = str()
-    if pre_item_html.find('author>') > -1:
-        temp_code = pre_item_html[pre_item_html.find('author>') + 7:]
-        author_of_podcast = check_on_shit(temp_code[:temp_code.find('</')])
-
-    # находим категории если они есть
-    categorys_of_podcast, subcategorys_of_podcast = parse_category(pre_item_html)
-
-    # print('Название: ' + title_of_podcast + '\n',
-    #       'Описание: ' + description_of_podcast + '\n',
-    #       'Картинка: ' + image_of_podcasts + '\n',
-    #       'Ключевые слова: ' + keyword_of_podcasts + '\n',
-    #       'Автор: ' + author_of_podcast + '\n',
-    #       'Категории: ' + categorys_of_podcast + '\n',
-    #       'Подкатегории: ' + subcategorys_of_podcast + '\n',)
-
-    # tags = (subcategorys_of_podcast, keyword_of_podcasts)
-    util.set_new_podcast(title_of_podcast, description_of_podcast, categorys_of_podcast, image_of_podcasts, author_of_podcast, subcategorys_of_podcast, keyword_of_podcasts)
+            Берем с бд все ссылки на подкасты, проходимся по ним, если у подкаста метка 0, то не качаем его инфу,
+        а сразу переходим к выпускам, иначе, качаем и инфу.
 
     """
-        Далее идем к выпускам подкаста, именуется этот тег в rss как item, и его столько сколько всего выпусков.
-        Имеем цикл, который ходит по этим тегам, из каждого тега выкачиваем ввсё что в нём есть.
-    """
+    for each_podcast in util.check_new_podcast():   # проходимся по ВСЕМ подкастам
+        html = requests.get(each_podcast.get('url_of_podcast')).content.decode('utf-8')
+        if each_podcast.get('download') == 1:
+            pre_item_html = html[:html.find('<item>')]
+            # находим название подкаста
+            title_of_podcast = pre_item_html[pre_item_html.find('<title>') + 7: pre_item_html.find('</title>')]
+            title_of_podcast = check_on_shit(title_of_podcast)
 
-    html = html[html.find('<item>'):]   # так как информация о подкасте уже собрана, обрезаем её и начинаем собирать инфу о выпусках
+            # находим описание подкаста
+            description_of_podcast = parse_description(pre_item_html)
 
-    while html.find('<item>') > -1:    # до тех пор пока находим новый выпуск
+            # находим картинку подкаста
+            if pre_item_html.find('<image>') > -1:
+                image_of_podcasts = pre_item_html[pre_item_html.find('<image>') + 7: pre_item_html.find('</image>')]
+                image_of_podcasts = image_of_podcasts[image_of_podcasts.find('<url>') + 5: image_of_podcasts.find('</url>')]
+            else:
+                image_of_podcasts = pre_item_html[pre_item_html.find('image') + 5:]
+                image_of_podcasts = image_of_podcasts[image_of_podcasts.find('href="') + 6:]
+                image_of_podcasts = image_of_podcasts[: image_of_podcasts.find('"')]
 
-        # получаем блок с этим itemом, чтоб работать не по всей странице
-        item_code = html[html.find('<item>') + 7: html.find('</item>')]
+            # находим ключевые слова если они есть
+            keyword_of_podcasts = str()
+            if pre_item_html.find('keywords>') > -1:     # если есть ключевые слова
+                keyword_of_podcasts = parse_keywords(pre_item_html)
 
-        # получаем название выпуска
-        title_of_item = item_code[item_code.find('<title>') + 7: item_code.find('</title>')]
-        title_of_item = check_on_shit(title_of_item)
+            # находим автора, если он есть
+            author_of_podcast = str()
+            if pre_item_html.find('author>') > -1:
+                temp_code = pre_item_html[pre_item_html.find('author>') + 7:]
+                author_of_podcast = check_on_shit(temp_code[:temp_code.find('</')])
 
-        # получаем описание выпуска
-        description_of_item = parse_description(item_code)
+            # находим категории если они есть
+            categorys_of_podcast, subcategorys_of_podcast = parse_category(pre_item_html)
 
-        # переходим в тег с ссылкой на аудио
-        enclosure = item_code[item_code.find('<enclosure'):]
-        enclosure = enclosure[enclosure.find('url="') + 5:enclosure.find('/>')]
-        mp3 = enclosure[: enclosure.find('"')]    # получаем аудио
+            # print('Название: ' + title_of_podcast + '\n',
+            #       'Описание: ' + description_of_podcast + '\n',
+            #       'Картинка: ' + image_of_podcasts + '\n',
+            #       'Ключевые слова: ' + keyword_of_podcasts + '\n',
+            #       'Автор: ' + author_of_podcast + '\n',
+            #       'Категории: ' + categorys_of_podcast + '\n',
+            #       'Подкатегории: ' + subcategorys_of_podcast + '\n',)
 
-        # получаем дату публикации выпуска
-        pubdata_of_item = clear_pubdata(item_code[item_code.find('<pubDate>') + 14: item_code.find('</pubDate>') - 6])
+            util.set_new_podcast(html, title_of_podcast, description_of_podcast, categorys_of_podcast,
+                                 image_of_podcasts, author_of_podcast, subcategorys_of_podcast, keyword_of_podcasts)
 
-        # получаем область с длительностью аудио
-        duration_of_item = str()
-        if item_code.find('duration>') > -1:
-            temp_code = item_code[item_code.find('duration>') + 9: item_code.find('duration>') + 20]
-            duration_of_item = temp_code[:temp_code.find('</')]    # получаем длительность аудио
-            if duration_of_item and duration_of_item.find(':') == -1:     # проверяем разделено ли время : (иначе оно указано в секундах)
-                duration_of_item = convert_of_time(int(duration_of_item))
-            duration_of_item = duration_of_item.replace(':', '')
+        """
+            Далее идем к выпускам подкаста, именуется этот тег(в плане сам выпуск) в rss как item, 
+            и его столько сколько всего выпусков.
+            Имеем цикл, который ходит по этим тегам, из каждого тега выкачиваем ввсё что в нём есть.
+        """
 
-        # получаем картинку выпуска если такова есть
-        image_of_item = str()
-        if item_code.find('image ') > -1 and item_code.find('"image"') == -1:
-            temp_code = item_code[item_code.find('image ') + 6:]
-            temp_code = temp_code[temp_code.find('href="') + 6:]
-            image_of_item = temp_code[: temp_code.find('"')]
+        html = html[html.find('<item>'):]   # обрезаем весь html до item
 
-        categorys_of_item, subcategorys_of_item = parse_category(item_code[:item_code.find('</item>')])
+        while html.find('<item>') > -1:    # до тех пор пока находим новый выпуск
 
-        # находим ключевые слова если они есть
-        keyword_of_item = str()
-        if item_code.find('keywords>') > -1:  # если есть ключевые слова
-            keyword_of_item = parse_keywords(item_code[:item_code.find('</item>')])
+            # получаем блок с этим itemом, чтоб работать не по всей странице
+            item_code = html[html.find('<item>') + 7: html.find('</item>')]
 
-        tags = (subcategorys_of_item, keyword_of_item)
+            # получаем название выпуска
+            title_of_item = item_code[item_code.find('<title>') + 7: item_code.find('</title>')]
+            title_of_item = check_on_shit(title_of_item)
 
-        html = html[html.find('</item>') + 7:]   # режем ту строку с которой отработали, и идем далее
-        # print('Название выпуска: ' + title_of_item + '\n',
-        #       'Описание выпуска: ' + description_of_item + '\n',
-        #       'Музыка: ' + mp3 + '\n',
-        #       'Дата публикации выпуска: ' + pubdata_of_item + '\n',
-        #       'Длительность выпуска: ' + duration_of_item + '\n',
-        #       'Картинка выпуска: ' + image_of_item + '\n',
-              # 'Категории выпуска: ' + categorys_of_item + '\n',
-              # 'Подкатегории выпуска: ' + subcategorys_of_item + '\n',
-              # 'Ключевые слова выпуска: ' + keyword_of_item + '\n',
-              # )
+            # получаем описание выпуска
+            description_of_item = parse_description(item_code)
+
+            # переходим в тег с ссылкой на аудио
+            enclosure = item_code[item_code.find('<enclosure'):]
+            enclosure = enclosure[enclosure.find('url="') + 5:enclosure.find('/>')]
+            mp3 = enclosure[: enclosure.find('"')]    # получаем аудио
+
+            # получаем дату публикации выпуска
+            pubdata_of_item = clear_pubdata(item_code[item_code.find('<pubDate>') + 14: item_code.find('</pubDate>') - 6])
+
+            # получаем область с длительностью аудио
+            duration_of_item = str()
+            if item_code.find('duration>') > -1:
+                temp_code = item_code[item_code.find('duration>') + 9: item_code.find('duration>') + 20]
+                duration_of_item = temp_code[:temp_code.find('</')]    # получаем длительность аудио
+                if duration_of_item and duration_of_item.find(':') == -1:     # проверяем разделено ли время : (иначе оно указано в секундах)
+                    duration_of_item = convert_of_time(int(duration_of_item))
+                duration_of_item = duration_of_item.replace(':', '')
+
+            # получаем картинку выпуска если такова есть
+            image_of_item = str()
+            if item_code.find('image ') > -1 and item_code.find('"image"') == -1:
+                temp_code = item_code[item_code.find('image ') + 6:]
+                temp_code = temp_code[temp_code.find('href="') + 6:]
+                image_of_item = temp_code[: temp_code.find('"')]
+
+            categorys_of_item, subcategorys_of_item = parse_category(item_code[:item_code.find('</item>')])
+
+            # находим ключевые слова если они есть
+            keyword_of_item = str()
+            if item_code.find('keywords>') > -1:  # если есть ключевые слова
+                keyword_of_item = parse_keywords(item_code[:item_code.find('</item>')])
+
+            tags = (subcategorys_of_item, keyword_of_item)
+
+            html = html[html.find('</item>') + 7:]   # режем ту строку с которой отработали, и идем далее
+            # print('Название выпуска: ' + title_of_item + '\n',
+            #       'Описание выпуска: ' + description_of_item + '\n',
+            #       'Музыка: ' + mp3 + '\n',
+            #       'Дата публикации выпуска: ' + pubdata_of_item + '\n',
+            #       'Длительность выпуска: ' + duration_of_item + '\n',
+            #       'Картинка выпуска: ' + image_of_item + '\n',
+            #       'Категории выпуска: ' + categorys_of_item + '\n',
+            #       'Подкатегории выпуска: ' + subcategorys_of_item + '\n',
+            #       'Ключевые слова выпуска: ' + keyword_of_item + '\n',
+            #       )
 
 
 if __name__ == '__main__':
@@ -221,7 +226,7 @@ if __name__ == '__main__':
     # parse('https://mojomedia.ru/feed-podcasts/dikie-utki')
     # parse('http://feeds.feedburner.com/DariaSadovaya')
     # parse('http://feeds.soundcloud.com/users/soundcloud:users:328939120/sounds.rss')
-    parse('http://feeds.soundcloud.com/users/soundcloud:users:132344904/sounds.rss')
+    # parse('http://feeds.soundcloud.com/users/soundcloud:users:132344904/sounds.rss')
     # parse('http://feeds.feedburner.com/pod24fps')
     # parse('https://anchor.fm/s/6f169f8/podcast/rss')
     # parse('http://feeds.soundcloud.com/users/soundcloud:users:679508342/sounds.rss')
--- util.py
+++ util.py
@@ -47,26 +47,65 @@ def execute(sql, *args, commit=False):
         return ans
 
 
-def set_new_podcast(title_of_podcast, description_of_podcasts, category_of_podcast,url_of_image_of_podcast, author_of_podcast, tags_of_podcast):
-    # print(title_of_podcast, description_of_podcasts, url_of_image_of_podcast, author_of_podcast, tags_of_podcast)
-    print(execute('SELECT title_of_podcast FROM podcasts WHERE title_of_podcast = %(p)s', title_of_podcast))
-    quit()
+def check_new_podcast():
+    return execute('SELECT * FROM url_of_podcasts')
 
-    execute('INSERT INTO podcasts (title_of_podcast, description_of_podcast, category_of_podcast, url_of_image_of_podcast, author_of_podcast) '
-            'VALUES (%(p)s, %(p)s, %(p)s, %(p)s, %(p)s)', title_of_podcast, description_of_podcasts, category_of_podcast, url_of_image_of_podcast, author_of_podcast,
+
+def set_new_podcast(url_of_podcast, title_of_podcast, description_of_podcasts, category_of_podcast,
+                    url_of_image_of_podcast, author_of_podcast, subcat_of_podcast, keyword_of_podcast):
+    if execute('SELECT title_of_podcast FROM podcasts WHERE title_of_podcast = %(p)s', title_of_podcast):   # проверяем есть ли подкаст уже в бд
+        return
+    execute('UPDATE url_of_podcasts SET download = 0 WHERE url_of_podcast = %(p)s', url_of_podcast, commit=True)    # ставим подкасту 0, что мы его уже скачали
+
+    execute('INSERT INTO podcasts (title_of_podcast, description_of_podcast, url_of_image_of_podcast, author_of_podcast) '  # добавляем новый подкаст
+            'VALUES (%(p)s, %(p)s, %(p)s, %(p)s)', title_of_podcast, description_of_podcasts, url_of_image_of_podcast, author_of_podcast,
             commit=True)
 
-    for list_of_tags in tags_of_podcast:
-        for eachtag in list_of_tags:
-            if eachtag:
-                id_of_podcast = execute('SELECT id_of_podcast FROM podcasts WHERE title_of_podcast = "(%(p)s)"', title_of_podcast)[0].get('id_of_podcast')
-                execute('INSERT INTO tags_of_podcasts () VALUES (%(p)s, %(p)s)', id_of_podcast, eachtag, commit=True)
+    # получаем id нового подкаста для скачки тегов и категорий
+    id_of_new_podcast = execute('SELECT id_of_podcast FROM podcasts WHERE title_of_podcast = %(p)s',
+                                title_of_podcast)[0].get('id_of_podcast')
 
-    print('Записал')
+    # проходимся по всем категориям, если такой нет записываем в категории, и соединяем с подкастом, иначе просто соединяем с подкастом
+    for eachcategory in category_of_podcast:
+        if not eachcategory:    # после обрезки, последний элемент - всегда пуст
+            break
+        if not execute('SELECT id_of_category FROM categorys WHERE title_of_category = %(p)s', eachcategory):
+            execute('INSERT INTO categorys(title_of_category) VALUES (%(p)s)', eachcategory, commit=True)   # если нет такой категории, создаем
+            id_of_new_category = execute('SELECT id_of_category FROM categorys '
+                                         'WHERE title_of_category = %(p)s',
+                                         eachcategory)[0].get('id_of_category')  # находим новую категорию и записываем её
+            execute('INSERT INTO podcasts_with_categorys(id_of_podcast, id_of_category) '
+                    'VALUES (%(p)s, %(p)s)', id_of_new_podcast, id_of_new_category, commit=True)    # привязываем подкаст к этой категории
+        else:
+            id_of_category = execute('SELECT id_of_category FROM categorys WHERE title_of_category = %(p)s',
+                                     eachcategory)[0].get('id_of_category')
+            execute('INSERT INTO podcasts_with_categorys(id_of_category, id_of_podcast) VALUES (%(p)s, %(p)s)',
+                    id_of_category, id_of_new_podcast, commit=True)
+
+    for eachsubcategory in subcat_of_podcast:   # добавляем подкатегории к подкасту
+        if eachsubcategory:   # во время срезки выходит пустая строка, доп проверка на неё
+            execute('INSERT INTO subcat_of_podcast(id_of_podcast, title_of_subcat) VALUES (%(p)s, %(p)s)',
+                    id_of_new_podcast, eachsubcategory, commit=True)
+
+    for eachkeyword in keyword_of_podcast:  # тот же алгоритм что и с категориями
+        if not eachkeyword:
+            break
+        if not execute('SELECT id_of_keyword FROM keywords WHERE title_of_keyword = %(p)s', eachkeyword):
+            execute('INSERT INTO keywords (title_of_keyword) VALUES (%(p)s)', eachkeyword, commit=True)
+            id_of_new_keyword = execute('SELECT id_of_keyword FROM keywords WHERE title_of_keyword = %(p)s',
+                                        eachkeyword)[0].get('id_of_keyword')
+            execute('INSERT INTO podcasts_with_keywords (id_of_podcast, id_of_keyword) VALUES (%(p)s, %(p)s)',
+                    id_of_new_podcast, id_of_new_keyword, commit=True)
+        else:
+            id_of_keyword = execute('SELECT id_of_keyword FROM keywords WHERE title_of_keyword = %(p)s',
+                                    eachkeyword)[0].get('id_of_keyword')
+            execute('INSERT INTO podcasts_with_keywords (id_of_podcast, id_of_keyword) VALUES (%(p)s, %(p)s)',
+                    id_of_new_podcast, id_of_keyword, commit=True)
 
 
 def set_new_item(title_of_podcast, title_of_audio, description_of_audio, audio, image_of_audio, pubdata_of_audio, duration_of_audio):
     id_of_podcast = execute('SELECT id_of_podcast FROM podcasts WHERE title_of_podcast = %(p)s', title_of_podcast)[0].get('id_of_podcast')
-    execute('INSERT INTO audio (id_of_podcast, titile_of_audio, description_of_audio, audio, image_of_audio, pubdata_of_audio, duration_of_audio)'
+    execute('INSERT INTO items (id_of_podcast, title_of_audio, description_of_audio, audio, image_of_audio, pubdata_of_audio, duration_of_audio)'
             ' VALUES (%(p)s, %(p)s, %(p)s, %(p)s, %(p)s, %(p)s, %(p)s)', id_of_podcast, title_of_audio, description_of_audio, audio, image_of_audio,
             pubdata_of_audio, duration_of_audio, commit=True)
+
